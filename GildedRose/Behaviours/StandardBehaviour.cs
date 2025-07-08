f1dae1568b64766c54d04be47ff7434fab5eed2e
joshua.partington <joshua.partington@brighthr.com>
2025-07-08 14:48:40 +0100

    Finished initial Refactor, to original rules

--- GildedRose/Behaviours/StandardBehaviour.cs
+++ GildedRose/Behaviours/StandardBehaviour.cs
@@ -4,18 +4,16 @@ public class StandardBehaviour : IBehavior
 {
     public void UpdateQuality(Item item)
     {
-        item.SellIn -= 1;
-
+       item.SellIn -= 1;
+        
         if (item.Quality > 0)
         {
-            if (item.SellIn < 0)
-            {
-                item.Quality -= 2;
-            }
-            else
-            {
-                item.Quality -= 1;
-            }           
+            item.Quality -= 1;
+        }
+        
+        if (item.SellIn < 0 && item.Quality > 0)
+        {
+            item.Quality -= 1;
         }
     }
 }
--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -7,15 +7,6 @@ public class GildedRose
     IList<Item> Items;
     ItemBehaviourProvider itemBehaviourProvider;
 
-    private readonly List<string> _transitionedItemBehaviours = new List<string>
-    {
-        ItemNames.AgedBrie,
-        ItemNames.DexterityVest,
-        ItemNames.ElixirOfTheMongoose,
-        ItemNames.BackstagePasses,
-        ItemNames.Sulfuras
-    };
-
     public GildedRose(IList<Item> Items)
     {
         this.Items = Items;
@@ -28,83 +19,8 @@ public class GildedRose
         {
             var item = Items[i];
 
-            if (_transitionedItemBehaviours.Contains(item.Name))
-            {
-                var behaviour = itemBehaviourProvider.GetBehaviour(item);
-                behaviour.UpdateQuality(item);
-            }
-            else
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
+            var behaviour = itemBehaviourProvider.GetBehaviour(item);
+            behaviour.UpdateQuality(item);
         }
     }
 }
